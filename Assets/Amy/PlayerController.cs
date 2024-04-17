5320ae9240b16cd7ef6ae0363c01c5552a62e3d7
syddixon <156958708+syddixon@users.noreply.github.com>
2024-04-17 09:47:07 -0400

    updated walking

--- Assets/Amy/PlayerController.cs
+++ Assets/Amy/PlayerController.cs
@@ -4,53 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float speed = 1;
+    //[SerializeField] float speed = 1;
     [SerializeField] float forceX = 1;
-    [SerializeField] float forceY = 1;
+   // [SerializeField] float forceY = 1;
     Rigidbody2D rb;
-    bool onGround = false;
+   // bool onGround = false;
+    Animator animator; //declare variable
+   // SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>(); //declare component
+       // spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (onGround)
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                rb.AddForce(forceX * Vector2.right);
-            }
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                rb.AddForce(forceX * Vector2.left);
-            }
-            if (Input.GetKey(KeyCode.Space))
-            {
-                rb.AddForce(forceY * Vector2.up);
-            }
+            rb.AddForce(forceX * Vector2.right);
+            //animator.SetBool("WalkRight", true);
         }
+           /* else
+            {
+                animator.SetBool("IdleFront", true);
+            }
+        if (Input.GetKey(KeyCode.Space)) 
+        {
+            rb.AddForce(forceY * Vector2.up);
+        }
+*/
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        //check if we collide with the ground
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            onGround = true;
-
-        }
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-        {
-        //check if we collide with the ground
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            onGround = false;
-
-        }
-        }
 }
+
